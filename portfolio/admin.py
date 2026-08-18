0d9042fc74cbf2dff8561828696e3ce5d44a13b0
unknown <jaya.maheswari.lakshmi@gmail.com>
2026-08-18 17:46:32 +0530

    Add upload error logging

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+import traceback
+
 from .models import Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+
     list_display = (
         "name",
         "designation",
         "email",
         "phone",
     )
+
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+        except Exception as e:
+            print("===== CLOUDINARY UPLOAD ERROR =====")
+            print(type(e).__name__)
+            print(str(e))
+            traceback.print_exc()
+            print("===== END CLOUDINARY UPLOAD ERROR =====")
+            raise
