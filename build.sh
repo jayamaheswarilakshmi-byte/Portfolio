ffa30b85b12542bf05fbece8519be8617c71f447
unknown <jaya.maheswari.lakshmi@gmail.com>
2026-08-20 08:00:16 +0530

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
-
 python manage.py migrate
-python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin'); password = os.environ.get('DJANGO_SUPERUSER_PASSWORD'); user, created = User.objects.get_or_create(username=username); user.set_password(password); user.is_staff=True; user.is_superuser=True; user.save()"
+2. Make build.sh Executable
+If Git didn't track execute permissions for the script, Render will throw a Permission Denied error during deployment.
+
+Run this in your local terminal, commit, and push:
+
+Bash
+chmod +x build.sh
+git add build.sh
+git commit -m "Make build.sh executable"
+git push origin main
