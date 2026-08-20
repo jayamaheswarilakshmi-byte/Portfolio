5f50366134042b1c45c4e1db350f72f586460e42
unknown <jaya.maheswari.lakshmi@gmail.com>
2026-08-20 08:11:24 +0530

    settings

--- build.sh
+++ build.sh
@@ -5,13 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-2. Make build.sh Executable
-If Git didn't track execute permissions for the script, Render will throw a Permission Denied error during deployment.
-
-Run this in your local terminal, commit, and push:
-
-Bash
-chmod +x build.sh
-git add build.sh
-git commit -m "Make build.sh executable"
-git push origin main
